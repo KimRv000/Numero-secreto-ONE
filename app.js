8301a1cbf7783b168b92901fc9429649d20a39ac
Kimberly Rv <elliis.rv@gmail.com>
2024-06-20 12:50:48 -0600

    [ADD] version 1.2.0

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ alert("Bienvenido(a) a Número Secreto")
 
 const numSecreto = 3;
 console.log(numSecreto);
+let resultado = "";
+let mensaje = "";
 
-const numUsuario = parseInt(prompt("Ingrese un número del 1 al 10"));
+
+let numUsuario = 0;
 console.log(numUsuario);
 
-if (numUsuario === numSecreto){
-    alert("Correcto!")
-} else{
-    alert("Incorrecto!")
+let intentos = 0;
+
+
+while (resultado !== "Correcto!") {
+    numUsuario = parseInt(prompt("Ingrese un número del 1 al 10"));
+    if (numUsuario < 0 || numUsuario > 10) {
+        alert("Número fuera de rango")
+    }
+    else if (numUsuario === numSecreto) {
+        resultado = "Correcto!";
+        alert(resultado)
+        alert(`Adivinaste en ${intentos} intentos, el número secreto es ${numSecreto}`)
+        break
+    } else {
+        if (numUsuario < numSecreto) {
+            mensaje = "Incorrecto, escoge un número mayor"
+            alert(mensaje)
+        } else {
+            mensaje = "Incorrecto, escoge un número menor"
+            alert(mensaje)
+        }
+    }
+    intentos += 1
 }
+
